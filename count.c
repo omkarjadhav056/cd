62a6672ec049efbed3b37534d8b401a6fb5ab009
omkarjadhav056 <118258648+omkarjadhav056@users.noreply.github.com>
2024-06-04 23:46:26 +0530

    Update count.c

--- count.c
+++ count.c
@@ -1,46 +1,39 @@
+%option noyywrap
+%{
 #include <stdio.h>
-#include <ctype.h>
 
-void countFile(FILE *fp, int *lines, int *tabs, int *spaces, int *words, int *chars) {
-    char c;
-    int inWord = 0;
-    *lines = *tabs = *spaces = *words = *chars = 0;
+int line_count = 0;
+int tab_count = 0;
+int space_count = 0;
+int word_count = 0;
+int character_count = 0;
+%}
 
-    while ((c = fgetc(fp)) != EOF) {
-        (*chars)++;
-        if (c == '\n') {
-            (*lines)++;
-        } else if (c == '\t') {
-            (*tabs)++;
-        } else if (c == ' ') {
-            (*spaces)++;
-        }
+%%
 
-        if (isspace(c)) {
-            inWord = 0;
-        } else if (!inWord) {
-            inWord = 1;
-            (*words)++;
-        }
-    }
-}
+[\t]         { tab_count++; character_count++; }
+[ ]+          { space_count += yyleng; character_count += yyleng; }
+\n            { line_count++; character_count++; }
+[a-zA-Z]+    { word_count++; character_count += yyleng; }
+.             { character_count++; }
+
+%%
 
 int main() {
-    FILE *fp = fopen("input.txt", "r");
-    if (fp == NULL) {
-        printf("Could not open file.\n");
+    yyin = fopen("input.txt", "r");
+    if (yyin == NULL) {
+        perror("fopen");
         return 1;
     }
 
-    int lines, tabs, spaces, words, chars;
-    countFile(fp, &lines, &tabs, &spaces, &words, &chars);
+    yylex();
 
-    printf("Lines: %d\n", lines);
-    printf("Tabs: %d\n", tabs);
-    printf("Spaces: %d\n", spaces);
-    printf("Words: %d\n", words);
-    printf("Characters: %d\n", chars);
+    printf("\nLines: %d\n", line_count);
+    printf("Tabs: %d\n", tab_count);
+    printf("Spaces: %d\n", space_count);
+    printf("Words: %d\n", word_count);
+    printf("Characters: %d\n", character_count);
 
-    fclose(fp);
+    fclose(yyin);
     return 0;
 }
